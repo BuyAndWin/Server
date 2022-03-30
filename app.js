a5a91359ac3be2b777659420dfdb3e30b504a9b3
RikiHildesaimmer <riki4100475@gmail.com>
2022-03-30 11:34:16 +0300

    arrange server

--- app.js
+++ app.js
@@ -1,17 +1,15 @@
 const mongoose = require("mongoose");
 const express = require("express");
-const itemsRout = require("./routes/items");
-const userRout = require("./routes/user");
-const winningRout = require("./routes/winning");
+const itemsRout = require("./Routes/items");
+const userRout = require("./Routes/user");
+const winningRout = require("./Routes/winning");
 const app = express();
 
 
-mongoose.connect("mongodb://localhost:27017/myDB").then(() => {
-    console.log("connted to mongo db")
-})
+mongoose.connect("mongodb://localhost:27017/myDB")
+    .then(() => { console.log("connted to mongo db") })
     .catch(err => console.log(err));
 
-    
 app.use(express.json())
 app.use("/items", itemsRout)
 app.use("/user", userRout)
--- controllers/itemsController.js
+++ controllers/itemsController.js
@@ -1,46 +1,49 @@
-const Item=require=("../models/itemsModels").Item;
+const Item = require = ("../models/itemsModel").Item;
 
 //שליפת כל המוצרים
-const getallItems=async(req,res)=>{
-    try{
-        let item=await Item.find({});
+const getallItems = async (req, res) => {
+    try {
+        let item = await Item.find({});
         return res.send(item);
     }
-    catch(e){
+    catch (e) {
         return res.status(400).send(e);
     }
 }
 //שליפת מוצר לפי id
-const getItemById=async(req,res)=>{
-    try{
-        let item=await Item.findByid({id:req.id});
-        return res.send(item);
+const getItemById = async (req, res) => {
+    try {
+        let item = await Item.findByid({ id: req.id });
+        return res.send(item); return res.send(item);
     }
-    catch(e){
+    catch (e) {
         return res.status(400).send(e);
     }
 }
 //הוספת מוצר
-const addItem=async(req,res)=>{
-    try{
-        let item=new Item(req.body);
+const addItem = async (req, res) => {
+    try {
+        let item = new Item(req.body);
         await item.save();
+        console.log(item);
         return res.send(item);
     }
-    catch(e)
-    {
-         return res.status(400).send(e);
+    catch (e) {
+        return res.status(400).send(e);
     }
 }
+
+getallItems();
+
 //מחיקת מוצר
-const deleteItem=async(req,res)=>{
-    let itemId=req.params.id;
-    try{
-        let item=await Item.findByIdAndDelete(itemId);
+const deleteItem = async (req, res) => {
+    let itemId = req.params.id;
+    try {
+        let item = await Item.findByIdAndDelete(itemId);
         return res.send(item);
     }
     catch{
         return res.status(400).send(e);
     }
 }
-module.exports={getallItems,getItemById,addItem,deleteItem}
+module.exports = { getallItems, getItemById, addItem, deleteItem }
